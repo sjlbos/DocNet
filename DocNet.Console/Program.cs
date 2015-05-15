d245938b5c558ef6cc514a9bc0cd2850813b62ad
sjlbos <sjlbos@outlook.com>
2015-05-14 18:52:44 -0700

    Razor and Log4Net updates
    - Added Razor NuGet packages to the DocNet.Core project
    - Added empty Razor view template
    - Filled in the log4net appenders in log4net.config

--- DocNet.Console/Program.cs
+++ DocNet.Console/Program.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using log4net;
 
 namespace DocNet.Console
 {
     public class Program
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Program));
+
         static void Main(string[] args)
         {
+            Log.Info("Welcome to DocNet!");
+            System.Console.ReadLine();
         }
     }
 }
--- DocNet.Console/Properties/AssemblyInfo.cs
+++ DocNet.Console/Properties/AssemblyInfo.cs
@@ -22,3 +22,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
+
+// Register log4net configuration
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
