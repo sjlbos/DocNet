e47d4fd43e998e98a23a7fb23d5c7d52b9c19422
KRollans <keith_rollans@hotmail.com>
2015-06-30 11:54:35 -0700

    Removed Glyphicon References
    Could not get the font to display correctly, removed it for now

--- DocNet.Core/Markup/js/custom.js
+++
@@ -1,9 +0,0 @@
-function hide(obj) {	
-	
-	if(obj.parentElement.firstElementChild.innerHTML == '<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>'){
-		obj.parentElement.firstElementChild.innerHTML = '<span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span>';
-	}
-	else {
-		obj.parentElement.firstElementChild.innerHTML = '<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>';
-	}
-}
