2f8ec5cf164f2fd9d77ea69edf1103eb31c0179a
sjlbos <sjlbos@outlook.com>
2015-07-15 11:13:49 -0700

    Filled in method stubs in documentation generator.

--- DocNet.Core/Output/Html/HtmlDocumentationGenerator.cs
+++ DocNet.Core/Output/Html/HtmlDocumentationGenerator.cs
@@ -46,26 +46,62 @@ namespace DocNet.Core.Output.Html
             // Process Child Interfaces
             foreach (var childInterface in currentNamespace.Interfaces)
             {
-
+                ProcessInterface(childInterface, outputDirectory);
             }
 
             // Process Child Classes
             foreach (var childClass in currentNamespace.Classes)
             {
-                
+                ProcessClass(childClass, outputDirectory);   
+            }
+
+            // Process Child Structs
+            foreach (var childStruct in currentNamespace.Structs)
+            {
+                ProcessStruct(childStruct, outputDirectory);
             }
 
             // Process Child Enums
             foreach (var childEnum in currentNamespace.Enums)
             {
-                
+                ProcessEnum(childEnum, outputDirectory);
             }
 
             // Process Child Delegates
             foreach (var childDelegate in currentNamespace.Delegates)
             {
-                
+                ProcessDelegate(childDelegate, outputDirectory);
             }
+        }
+
+        private void ProcessInterface(InterfaceModel interfaceModel, string outputDirectory)
+        {
+            string interfaceFilePath = Path.Combine(outputDirectory, interfaceModel.FullName + ".html");
+            WriteView<InterfaceDetail, InterfaceModel>(interfaceFilePath, interfaceModel);
+        }
+
+        private void ProcessClass(ClassModel classModel, string outputDirectory)
+        {
+            string classFilePath = Path.Combine(outputDirectory, classModel.FullName + ".html");
+            WriteView<ClassDetail, ClassModel>(classFilePath, classModel);
+        }
+
+        private void ProcessStruct(StructModel structModel, string outputDirectory)
+        {
+            string structFilePath = Path.Combine(outputDirectory, structModel.FullName + ".html");
+            WriteView<StructDetail, StructModel>(structFilePath, structModel);
+        }
+
+        private void ProcessEnum(EnumModel enumModel, string outputDirectory)
+        {
+            string enumFilePath = Path.Combine(outputDirectory, enumModel.FullName + ".html");
+            WriteView<EnumDetail, EnumModel>(enumFilePath, enumModel);
+        }
+
+        private void ProcessDelegate(DelegateModel delegateModel, string outputDirectory)
+        {
+            string delegateFilePath = Path.Combine(outputDirectory, delegateModel.FullName + ".html");
+            WriteView<DelegateDetail, DelegateModel>(delegateFilePath, delegateModel);
         }
 
         private void WriteView<TView, TModel>(string filePath, TModel model) where TView:BaseTemplate<TModel>, new()
